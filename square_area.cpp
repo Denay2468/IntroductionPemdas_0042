da8155566b459c4e623728d45928c031500acd96
Denay2468 <denayaung13@gmail.com>
2026-03-31 18:33:06 +0630

    code in main function

--- square_area.cpp
+++ square_area.cpp
@@ -2,5 +2,14 @@
 using namespace std ;
 
 int main(){
-    
+    int area,length,wide;
+
+    cout<<"ENTER THE LENGTH :";
+    cin>>length;
+
+    cout<<"ENTER THE WIDE :";
+    cin>>wide;
+
+    area = length * wide ;
+    cout<<"THE RESULT :"<<area;
 }
